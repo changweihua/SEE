cec29ec3802bc44a11e4a5a3595f8fb43b7fc196
Changweihua <changweihua@outlook.com>
2013-04-12 18:07:13 +0800

    初始化项目

---
+++ SEE/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SEE.Controllers
+{
+    public class UserController : Controller
+    {
+        public string Message { get; set; }
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = Message;
+            return View();
+        }
+
+    }
+}
---
+++ SEE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SEE.MvcApplication" Language="C#" %>
